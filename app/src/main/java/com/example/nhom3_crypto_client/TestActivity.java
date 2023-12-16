1ba9a7f6d6549e3412c01a2c58fe30b20c761885
Mini Lemon <quyto111@gmail.com>
2023-12-16 16:51:57 +0700

    test 1

--- app/src/main/java/com/example/nhom3_crypto_client/TestActivity.java
+++ app/src/main/java/com/example/nhom3_crypto_client/TestActivity.java
@@ -10,5 +10,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        //test quy branch
     }
 }
