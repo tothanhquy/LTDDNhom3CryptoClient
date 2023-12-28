e159f4e4793190657b0f05b2d508ce30028c6d97
Mini Lemon <quyto111@gmail.com>
2023-12-28 23:44:13 +0700

    create coin chart

--- app/src/main/java/com/example/nhom3_crypto_client/view/QuyMainActivity.java
+++ app/src/main/java/com/example/nhom3_crypto_client/view/QuyMainActivity.java
@@ -1,5 +1,7 @@
 package com.example.nhom3_crypto_client.view;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -10,9 +12,17 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 
 import com.example.nhom3_crypto_client.R;
+import com.example.nhom3_crypto_client.service.CoinService;
+import com.example.nhom3_crypto_client.service.ServiceConnections;
+import com.example.nhom3_crypto_client.service.ServiceCreatedCallback;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -21,6 +31,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuyMainActivity extends AppCompatActivity {
+    QuyMainActivityHomeFragment quyMainActivityHomeFragment;
+    QuyMainActivityInterestedCoinsFragment quyMainActivityInterestedCoinsFragment;
+    QuyMainActivityTradingFragment quyMainActivityTradingFragment;
+    QuyMainActivityProfileFragment quyMainActivityProfileFragment;
+
+
+    private String REGISTER_COIN_SERVICE_NAME = "coin-list";
+    private CoinService coinService;
+    private Boolean isBoundCoinService=false;
+    private ServiceConnection serviceConnection;
+
+    private class CoinServiceCreatedCallback implements ServiceCreatedCallback {
+        @Override
+        public void setService(Service service) {
+            coinService = (CoinService) service;
+        }
+        @Override
+        public void setIsBound(Boolean isBound) {
+            isBoundCoinService = isBound;
+        }
+        @Override
+        public void createdComplete() {
+
+        }
+    }
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -31,18 +66,39 @@ public class QuyMainActivity extends AppCompatActivity {
 
         String[] tabTitles = {"Home","Yêu thích" ,"Giao dịch", "Hồ sơ"};//put titles based on your need
         int[] tabIcons = {R.drawable.outline_info_24,R.drawable.outline_info_24, R.drawable.outline_info_24, R.drawable.outline_info_24};
-        ArrayList<Fragment> fragments = new ArrayList<>(Arrays.asList(new QuyMainActivityHomeFragment(), new QuyMainActivityInterestedCoinsFragment(), new QuyMainActivityTradingFragment(), new QuyMainActivityProfileFragment()));
+
+        quyMainActivityHomeFragment = new QuyMainActivityHomeFragment();
+        quyMainActivityInterestedCoinsFragment = new QuyMainActivityInterestedCoinsFragment();
+        quyMainActivityTradingFragment = new QuyMainActivityTradingFragment(getApplicationContext(),changeCoinLauncher);
+        quyMainActivityProfileFragment = new QuyMainActivityProfileFragment();
+
+        ArrayList<Fragment> fragments = new ArrayList<>(Arrays.asList(quyMainActivityHomeFragment, quyMainActivityInterestedCoinsFragment,quyMainActivityTradingFragment, quyMainActivityProfileFragment));
         ViewPager2 viewPager2 = findViewById(R.id.quyMainViewPager);
         viewPager2.setAdapter(new ViewPager2Adapter(getSupportFragmentManager(),getLifecycle(),viewPager2,fragments));
         TabLayout tabLayout = findViewById(R.id.quyMainTabLayout);
-
+        viewPager2.setUserInputEnabled(false);
         new TabLayoutMediator(tabLayout, viewPager2,
                 ((tab, position) -> {
                     tab.setText(tabTitles[position]);
                     tab.setIcon(tabIcons[position]);
                 }
             )).attach();
+
+        CoinServiceCreatedCallback serviceCreatedCallback = new CoinServiceCreatedCallback();
+        serviceConnection = new ServiceConnections.CoinServiceConnection(serviceCreatedCallback);
+        Intent intent = new Intent(this, CoinService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
+
+    ActivityResultLauncher<Intent> changeCoinLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(), o -> {
+            if (o.getResultCode()== Activity.RESULT_OK){
+                Intent intent = o.getData();
+                String coinId = intent.getStringExtra("coinId");
+                quyMainActivityTradingFragment.changeCoinChartView(coinId);
+                //changeCoinOkCallback.handle(coinId);
+            }
+        });
     private class ViewPager2Adapter extends FragmentStateAdapter {
         ViewPager2 viewPager2;
         List<Fragment> fragments;
---
+++ app/src/main/java/com/example/nhom3_crypto_client/view_model/QuyCoinChartViewModel.java
@@ -0,0 +1,49 @@
+package com.example.nhom3_crypto_client.view_model;
+
+import android.content.Context;
+
+import com.example.nhom3_crypto_client.api.API;
+import com.example.nhom3_crypto_client.model.SystemNotificationModel;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+
+public class QuyCoinChartViewModel extends BaseViewModel{
+    private Context context;
+
+    public QuyCoinChartViewModel(Context context){
+        this.context = context;
+    }
+
+    public void loadChart(String type, String coidId, String interval, String start, String end , BaseViewModel.OkCallback okCallback){
+        if(isLoading().getValue())return;
+        _isLoading.postValue(true);
+        new Thread(()->{
+            loadChartAPI(type,coidId,interval,start,end,okCallback);
+        }).start();
+    }
+
+    private void loadChartAPI(String type, String coidId, String interval, String start, String end ,BaseViewModel.OkCallback okCallback){
+        try{
+            API.RequestParams params = new API.RequestParams();
+            params.add("coinId",coidId);
+            params.add("interval",interval);
+            params.add("start",start);
+            params.add("end",end);
+
+            API.ResponseAPI response = API.get(context,"/coins/chart/"+type,params);
+            if(response.status== API.ResponseAPI.Status.Fail){
+                _notification.postValue(new SystemNotificationModel(SystemNotificationModel.Type.Error,response.error));
+            }else{
+                okCallback.handle(response.data);
+//                _notification.postValue(new SystemNotificationModel(SystemNotificationModel.Type.Info,"ok"));
+            }
+        }catch(Exception e){
+//            throw new RuntimeException(e);
+            System.out.println(e);
+            _notification.postValue(new SystemNotificationModel(SystemNotificationModel.Type.Error,"Lỗi hệ thống."));
+        }finally {
+            _isLoading.postValue(false);
+        }
+    }
+}
